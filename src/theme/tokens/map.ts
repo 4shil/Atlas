2ec895f7c16d3c08725557c60f6f167520eeba02
4shil <166588383+4shil@users.noreply.github.com>
2026-02-21 22:22:33 +0530

    map

---
+++ src/theme/tokens/map.ts
@@ -0,0 +1,78 @@
+/**
+ * Atlas Design System — Map Tokens
+ */
+
+import { rawColors } from './colors';
+
+export const cinematicDarkMapStyle = [
+    {
+        elementType: 'geometry',
+        stylers: [{ color: rawColors.gray[900] }],
+    },
+    {
+        elementType: 'labels.icon',
+        stylers: [{ visibility: 'off' }],
+    },
+    {
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[500] }],
+    },
+    {
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: rawColors.gray[900] }],
+    },
+    {
+        featureType: 'administrative',
+        elementType: 'geometry',
+        stylers: [{ color: rawColors.gray[500] }],
+    },
+    {
+        featureType: 'administrative.country',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[300] }],
+    },
+    {
+        featureType: 'administrative.land_parcel',
+        stylers: [{ visibility: 'off' }],
+    },
+    {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[300] }],
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[500] }],
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: rawColors.gray[900] }],
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: rawColors.gray[700] }],
+    },
+    {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[500] }],
+    },
+    {
+        featureType: 'transit',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[500] }],
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: rawColors.black }],
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: rawColors.gray[700] }],
+    },
+] as const;
